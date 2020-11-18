a4e9b320941d9966a680a0b76939d93689e856be
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2020-11-17 21:52:47 -0500

    add ability to download all available tournament results

--- scraper.py
+++ scraper.py
@@ -1,5 +1,10 @@
+import time
 import requests
+import os
+import logging
 from bs4 import BeautifulSoup
+
+logging.basicConfig(filename='scraper.log', level=logging.INFO)
 
 URL = r"https://unosmium.org/results/"
 page = requests.get(URL)
@@ -10,15 +15,25 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 cards = soup.find_all('div', class_='card')
 
-x = cards[0]
+for card in cards:
+    t = card.find("h2", class_='card-title').text.split()
+    title = "".join(t[:-2]).replace("/", "")
+    division = "".join(t[-2:])
+    link = card.find('a', href=True)['href'].split("/")
+    link = DATA(link[2])
 
+    path = os.path.join("tournaments", division, title) + '.yaml'
 
-title = "".join(x.find("h2", class_='card-title').text.split())
-print(title)
+    if not os.path.isfile(path):
+        with open(path, 'w+') as fout:
+            file = requests.get(link)
 
-# with open("tournaments/" + title, 'w+') as fout:
-link = x.find('a', href=True)['href'].split("/")
-link = DATA(link[2])
+            try:
+                fout.write(file.content.decode('utf-8'))
+            except UnicodeEncodeError as e:
+                logging.warning(f"could not download {path}")
+            else:
+                logging.info(f"downloaded {path}")
 
-with open("tournaments/" + title + ".yaml", 'w+') as fout:
-    fout.write(requests.get(link).content.decode("utf-8"))
+        time.sleep(3)
+
